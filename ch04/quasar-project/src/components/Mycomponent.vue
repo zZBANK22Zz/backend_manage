e1bf9ecd80e8ad472d7c3557847215b7c017dc09
bank <ananda2546@gmail.com>
2023-11-16 12:40:23 +0700

    hh

--- ch04/quasar-project/src/components/Mycomponent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h6>this is Mycomponent</h6>
-</template>
-<script>
-import { defineComponent } from 'vue';
-export default defineComponent({
-    name: "MyComponent"
-})
-</script>
--- ch04/quasar-project/src/components/Secondcomponent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h6>this is MySecondComponent</h6>
-</template>
-<script>
-import { defineComponent } from 'vue';
-export default defineComponent({
-    name: "MySecondComponent"
-})
-</script>
