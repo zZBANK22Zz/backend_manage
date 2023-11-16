079b3c6209b95035d0d593c75401429aa06dcb3c
bank <ananda2546@gmail.com>
2023-11-16 11:32:42 +0700

    anan

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    // HOST: "localhost",
-    // USER: "root",
-    // PASSWORD: "",
-    // DB: "myAPIs"
-    HOST: "aws.connect.psdb.cloud",
-    USER: "knnavq3wo64f5hsh7pqi",
-    PASSWORD: "pscale_pw_K9rwApwgxNZIRcyAPsWXXFa3QH8wFfmsgpuTRPF7sEV",
-    DB: "homework_5"
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DB: "myAPIs"
+    // HOST: "aws.connect.psdb.cloud",
+    // USER: "knnavq3wo64f5hsh7pqi",
+    // PASSWORD: "pscale_pw_K9rwApwgxNZIRcyAPsWXXFa3QH8wFfmsgpuTRPF7sEV",
+    // DB: "homework_5"
 }
