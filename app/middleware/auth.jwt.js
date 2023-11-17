aab3db99487d0cfa015eff51adce5da421725a5b
bank <ananda2546@gmail.com>
2023-11-17 21:47:40 +0700

    1122

--- app/middleware/auth.jwt.js
+++ app/middleware/auth.jwt.js
@@ -3,10 +3,10 @@ const scKey = require("../config/jwt.config");
 const verifyToken = (req, res, next)=>{
     const token = req.headers["x-access-token"];
     if(!token){
-        return res.status(403).send({massage: "NO token provided"});
+        return res.status(403).send({message: "No token provided"});
     }
     jwt.verify(token, scKey.secret, (err, decoded)=>{
-        if(err) return res.status(401).send({massage: "Unautherized"});
+        if(err) return res.status(401).send({message: "Unauthorized"});
         req.id = decoded.id;
         next();
     });
--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -2,17 +2,16 @@ const multer = require("multer")
 const util = require("util")
 const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
-        cb(null, __basedir+ "/assets/");
+        cb(null, __basedir + "/assets/");
     },
     filename: (req, file, cb)=>{
         const extArray = file.mimetype.split("/");
         const extension = extArray[extArray.length-1];
-        const newFilename = "FileUpload-"+Date.now()+"."+extension;
+        const newFilename = "Fileupload-"+Date.now()+"."+extension;
         cb(null, newFilename);
     }
 });
-// console.log("Middleware");
-const uploadFile = multer({storage: storage}).single("singlefile");
-//promisify => asyn-await
+const uploadFile = multer({ storage: storage}).single("singlefile");
+//promisify => async-await
 const uploadMiddleware = util.promisify(uploadFile);
 module.exports = uploadMiddleware;
